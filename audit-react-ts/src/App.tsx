175f3412c9faa0664c4b9c5d5866f8aa30ae156e
Aman Ullah <amanix007@gmail.com>
2021-12-04 03:09:37 +0600

    basic frontend created

--- audit-react-ts/src/App.tsx
+++ audit-react-ts/src/App.tsx
@@ -1,43 +1,48 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+
+import SaveIcon from '@mui/icons-material/Save';
 import './App.css'
+import { Button, TextField } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="site-entry-app">
+      <form >
+
+        <div className="audit-form">
+          <div className="audit-form-header">
+            <h1>Site Entry(Audit)</h1>
+            <div className="buttons">
+              <Button variant="outlined" startIcon={<SaveIcon />}>Save</Button>
+              <Button variant="outlined">Cancel</Button>
+            </div>
+          </div>
+          <div className="form-body">
+            <div className="input-field">
+              <TextField label="Name" fullWidth variant="outlined" />
+            </div>
+            <div className="input-field">
+              <TextField label="Jurishdiction/City/Region" fullWidth variant="outlined" />
+            </div>
+            <div className="input-field">
+              <TextField label="Site Description" fullWidth variant="outlined" />
+            </div>
+            <div className="input-field double-input">
+              <TextField label="Latitude" fullWidth variant="outlined" />
+              <TextField label="Longitude" fullWidth variant="outlined" />
+            </div>
+          </div>
+          <div className="audit-log">
+            <h4>Audit Log</h4>
+            <ul>
+              <li>Created by Simon on 2/1/2020, 12:00:00 AM</li>
+              <li>Updated by Nandita on 1/12/2021, 11:50:00 PM</li>
+            </ul>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
